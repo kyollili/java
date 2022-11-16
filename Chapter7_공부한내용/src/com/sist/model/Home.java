6e947246543fce66091b444b1df249bc8730508e
굘리리 <@LAPTOP-FE6336R9>
2022-11-16 12:35:32 +0900

    asdf

---
+++ Chapter7_공부한내용/src/com/sist/model/Home.java
@@ -0,0 +1,10 @@
+package com.sist.model;
+
+public class Home implements Move{
+
+	public void move()
+	{
+		System.out.println("홈으로 이동");
+	}
+
+}
---
+++ Chapter7_공부한내용/src/com/sist/model/Movie.java
@@ -0,0 +1,10 @@
+package com.sist.model;
+
+public class Movie implements Move{
+
+	public void move()
+	{
+		System.out.println("영화로 이동");
+	}
+
+}
